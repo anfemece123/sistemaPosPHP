8d10f9885865361fe65ca86fec92bac52d9a98e0
Andres Melo <andresfelipemelo18@gmail.com>
2023-05-15 16:21:14 -0500

    commit second

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,4 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2023 <a href="https://portfolio-six-phi-53.vercel.app/">Andres Melo "Desarrollador de software"</a>.</strong> All rights
+    reserved.
+  </footer>
